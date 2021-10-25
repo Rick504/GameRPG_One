2d53e931b21ab0bdf8c371f0b88f8f711c9abe15
Rick504 <rick.wes1@gmail.com>
2021-10-25 15:46:26 -0300

    Conecção realizada com sucesso ao banco Postgres, API de cadastro do usuario realizado com sucesso, agora preciso trabalhar no resto da aplicação, como criar login com o postgres e fazer rodar o game

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-// const bodyParser=require('body-parser')
-// const mysql=require('mysql2')
-// const handlebars=require('express-handlebars')
-// const urlencodeParser=bodyParser.urlencoded({extended:false})
+const handlebars=require('express-handlebars')
+const bodyParser=require('body-parser')
+const urlencodeParser=bodyParser.urlencoded({extended:false})
 const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
-// , session = require('express-session')
-
+const session = require('express-session')
 
 // //------------------------------------------------------------------------------------------------------------------------------------//
 
-// //modulo que carrega variaveis de ambiente em arquivo .env
-// // require('dotenv').config()
+//modulo que carrega variaveis de ambiente em arquivo .env
+// require('dotenv').config()
 
-// //MySql - settings
-// global.sql=mysql.createConnection({
-//     host: 'db',
-//     user: 'root',
-//     password: '',
-//     database: 'First_game'
-// })
-// module.exports = sql
-// sql.query("use First_game")
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db',
+  database: 'First_game',
+  password: '123',
+  port: 5432,
+})
 
-// //session - settings ---------------------------------------//
-// app.use(session({secret: 'secret',resave: true,saveUninitialized: true}))
+//session - settings ---------------------------------------//
+app.use(session({secret: 'secret',resave: true,saveUninitialized: true}))
 
 // //bodyParser - settings -------------------------------------//
-// app.use(bodyParser.urlencoded({extended : true}))
-// app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 // //handlebars - settings -------------------------------------//
 
-// app.set('view engine', 'handlebars')
-// app.engine('handlebars', handlebars({
-//     layoutsDir: __dirname + '/views/layouts',
-// }))
+app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}))
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 // //------------------------------------------------------------------------------------------------------------------------------------//
 
@@ -49,16 +46,27 @@ app.get('/',function(req,res) {
   })
   
   //Cadastrar / HTML - rota 
-  app.get('/register',function(req,res) {
+app.get('/register',function(req,res) {
    res.sendFile(path.join(__dirname+'/html/register.html'))
-  })
+})
 
+// API para ver resultados do Banco
+app.get('/psql', async(req, res) => {
+  const { rows } = await pool.query('select * from users')
+  res.send(rows)
+})
 
-// //Cadastro Realizado / HTML - api
-// app.post("/registrationPerformed",urlencodeParser,function(req,res){
-//     sql.query("INSERT INTO users (id,name,email,password,origin) values (?,?,?,?,?) ",[req.body.id, req.body.name, req.body.email, req.body.password, req.body.origin])
-//     res.sendFile(path.join(__dirname+'/html/registrationPerformed.html'))
-// })
+//Cadastro Realizado / HTML - api
+app.post("/registrationPerformed",urlencodeParser,function(req,res){
+  const { u_name, email, password, origin } = req.body
+
+  pool.query(`INSERT INTO users(u_name, email, password, origin) VALUES ($1, $2, $3, $4) RETURNING id;`,
+  [u_name,email,password,origin]), (error, results) => {
+    if (error) {
+      throw error
+    }}
+    res.sendFile(path.join(__dirname+'/html/registrationPerformed.html'))
+})
 
 // //------------------------------------------------------------------------------------------------------------------------------------//
 
--- db/psql_dump.sql
+++ db/psql_dump.sql
@@ -1,10 +1,10 @@
 -- Creation of product table
 CREATE TABLE IF NOT EXISTS users (
-    origin varchar(20) NOT NULL,
     id serial NOT NULL,
-    name varchar(45) NOT NULL,
+    u_name varchar(45) NOT NULL,
     email varchar(45) NOT NULL,
     password varchar(150) NOT NULL,
+    origin varchar(20) NOT NULL,
     gold varchar(10) DEFAULT 1 NOT NULL,
     wood varchar(10) DEFAULT 5 NOT NULL,
     supplies varchar(10) DEFAULT 5 NOT NULL,
