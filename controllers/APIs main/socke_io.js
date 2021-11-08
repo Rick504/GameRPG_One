2ddba6bdb8cd5c5aaa50a1803a256896cd480996
Rick504 <rick.wes1@gmail.com>
2021-11-08 16:39:39 -0300

    frontend, criei a pagina de comercio, a de inicil e a de trabalhadores , e editei algumas coisas no css , porem ainda precisa de retoques

--- controllers/APIs main/socke_io.js
+++ controllers/APIs main/socke_io.js
@@ -6,36 +6,36 @@ module.exports = function ( app ) {
 
     // AUMENTA RECURSOS DO JOGADOR POR TURNO AO DESCOMENTAR -------------------------------------------//
 
-    var prime = null
-    io.on('connection', function(socket){
+    // var prime = null
+    // io.on('connection', function(socket){
 
-        setInterval(() => {
+    //     setInterval(() => {
 
-            const currint_gold = parseInt(gold)  +  parseInt(worker_producing_gold)
-            const currint_supplies = parseInt(supplies)  +  parseInt(worker_producing_supplies)
-            const currint_wood = parseInt(wood)  +  parseInt(worker_producing_wood)
-
-
-            conn.query("UPDATE users SET gold = " + currint_gold +", supplies = "+ currint_supplies + ", wood = " + currint_wood + "  WHERE id = $1",[user_id])
-
-            //atualizando results do BD
-            gold = currint_gold
-            supplies = currint_supplies
-            wood = currint_wood
-
-            //atualizando handlebars
-            dados_user.gold = currint_gold
-            dados_user.supplies = currint_supplies
-            dados_user.wood = currint_wood
-
-        }, 10000)
+    //         const currint_gold = parseInt(gold)  +  parseInt(worker_producing_gold)
+    //         const currint_supplies = parseInt(supplies)  +  parseInt(worker_producing_supplies)
+    //         const currint_wood = parseInt(wood)  +  parseInt(worker_producing_wood)
 
 
-        setInterval(() => {
-            prime+=2
-        }, 5000)
-        socket.emit('cart', prime)
-    })
+    //         conn.query("UPDATE users SET gold = " + currint_gold +", supplies = "+ currint_supplies + ", wood = " + currint_wood + "  WHERE id = $1",[user_id])
+
+    //         //atualizando results do BD
+    //         gold = currint_gold
+    //         supplies = currint_supplies
+    //         wood = currint_wood
+
+    //         //atualizando handlebars
+    //         dados_user.gold = currint_gold
+    //         dados_user.supplies = currint_supplies
+    //         dados_user.wood = currint_wood
+
+    //     }, 10000)
+
+
+    //     setInterval(() => {
+    //         prime+=2
+    //     }, 5000)
+    //     socket.emit('cart', prime)
+    // })
 
     //------------------------------------------------------------------------------------------------//
 
