56d81954f8bf35ebd97cd364c3fbd75926cff329
rick <rick.wes1@gmail.com>
2022-01-25 16:56:00 -0300

    Atualizei as tabelas de exercito com nome de guerreiros e testei com relação criando e usando o campo id_army na tabela users e o id das tabelas dos guerreiros, tirei o name dos formularios que não estavam sendo utilizados e criei um arquivo de test.sql para guardar testes sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,8 +1,7 @@
--- table users
 CREATE TABLE IF NOT EXISTS users (
-    id serial NOT NULL,
+    id serial NOT NULL PRIMARY KEY,
     u_name varchar(45) UNIQUE NOT NULL,
-    email varchar(45) UNIQUE NOT NULL,
+    email varchar(50) UNIQUE NOT NULL,
     password varchar(150) NOT NULL,
     origin varchar(20) NOT NULL,
     gold varchar(10) DEFAULT 1 NOT NULL,
@@ -16,104 +15,92 @@ CREATE TABLE IF NOT EXISTS users (
     level varchar(2) DEFAULT 1 NOT NULL,
     cla varchar(30),
     tech_points varchar(2) DEFAULT 2 NOT NULL,
-    PRIMARY KEY (id)
+    id_army INT DEFAULT 1
 );
 
---------------Tabela origem Pesquisador
-CREATE TABLE warriors_researcher ( 
-    id serial NOT NULL, 
-    warrior VARCHAR(20), 
-    lvl int ,
-    atk VARCHAR(3),
-    def VARCHAR(3), 
-    origin VARCHAR(15) DEFAULT 'Researcher', 
-    total VARCHAR(10),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS warriors_researcher ( 
+    id INT PRIMARY KEY, 
+    warrior VARCHAR(50), 
+    level INT NOT NULL,
+    atk INT NOT NULL,
+    def INT NOT NULL
 );
 
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (1,'guerreiro-atk-1', 1,   '10', '5', 'Researcher', '0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (2,'guerreiro-def-1', 1,   '5', '10', 'Researcher', '0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (3,'guerreiro-atk-2', 2,   '17', '11', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (4,'guerreiro-def-2', 2,   '11', '17', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (5,'guerreiro-atk-3', 3,   '23', '18', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (6,'guerreiro-def-3', 3,   '18', '23', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (7,'guerreiro-atk-4', 4,   '30', '25', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (8,'guerreiro-def-4', 4,   '25', '30', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (9,'guerreiro-atk-5', 5,   '35', '31', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (10,'guerreiro-def-5',  5, '31', '35', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (11,'guerreiro-atk-6',  6, '40', '37', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (12,'guerreiro-def-6',  6, '37', '40', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (13,'guerreiro-atk-7',  7, '46', '42', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (14,'guerreiro-def-7',  7, '42', '46', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (15,'guerreiro-atk-8',  8, '65', '50', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (16,'guerreiro-def-8',  8, '50', '65', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (17,'guerreiro-atk-9',  9, '80', '70', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (18,'guerreiro-def-9',  9, '70', '80', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (19,'guerreiro-atk-10', 10,'100', '90', 'Researcher','0');
-INSERT INTO warriors_researcher (id,warrior,lvl,atk,def,origin, total) values (20,'guerreiro-def-10', 10,'90', '100', 'Researcher','0');
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (1,'Guerreiros do comandante Arzur de Atk', 1, 10, 5);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (2,'Guerreiros do comandante Arzur de Def', 1, 5, 10);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (3,'Guerreiros do comandante Cadman de Atk', 2, 17, 11);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (4,'Guerreiros do comandante Cadman de Def', 2, 11, 17);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (5,'Guerreiros do comandante Grannus de Atk', 3, 23, 18);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (6,'Guerreiros do comandante Grannus de Def', 3, 18, 23);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (7,'Guerreiros da comandante Hilda de Atk', 4, 30, 25);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (8,'Guerreiros da comandante Hilda de Def', 4, 25, 30);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (9,'Guerreiros do comandante Thyri de Atk', 5, 35, 31);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (10,'Guerreiros do comandante Thyri de Def', 5, 31, 35);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (11,'Guerreiros do comandante Alef de Atk', 6, 40, 37);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (12,'Guerreiros do comandante Alef de Def', 6, 37, 40);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (13,'Guerreiros do comandante Kentigern de Atk', 7, 46, 42);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (14,'Guerreiros do comandante Kentigern de Def', 7, 42, 46);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (15,'Guerreiros do comandante Eratos de Atk', 8, 65, 50);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (16,'Guerreiros do comandante Eratos de Def', 8, 50, 65);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (17,'Guerreiros do comandante Cingétorix de Atk', 9, 80, 70);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (18,'Guerreiros do comandante Cingétorix de Def', 9, 70, 80);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (19,'Guerreiros ELITE de Atk', 10, 100, 90);
+INSERT INTO warriors_researcher (id,warrior,level,atk,def) values (20,'Guerreiros ELITE de Def', 10, 90, 100);
 
---------------Tabela origem Magico
-CREATE TABLE warriors_magic ( 
-    id serial NOT NULL, 
-    warrior VARCHAR(20), 
-    lvl int ,
-    atk VARCHAR(3),
-    def VARCHAR(3), 
-    origin VARCHAR(15) DEFAULT 'Magic', 
-    total VARCHAR(10),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS warriors_magic ( 
+    id INT PRIMARY KEY, 
+    warrior VARCHAR(50), 
+    level INT NOT NULL,
+    atk INT NOT NULL,
+    def INT NOT NULL
 );
 
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (21,'guerreiro-atk-1', 1,  '8', '4', 'Magic', '0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (22,'guerreiro-def-1', 1,  '4', '8', 'Magic', '0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (23,'guerreiro-atk-2', 2,  '15', '10', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (24,'guerreiro-def-2', 2,  '10', '15', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (25,'guerreiro-atk-3', 3,  '20', '18', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (26,'guerreiro-def-3', 3,  '18', '20', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (27,'guerreiro-atk-4', 4,  '31', '21', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (28,'guerreiro-def-4', 4,  '21', '31', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (29,'guerreiro-atk-5', 5,  '33', '28', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (30,'guerreiro-def-5',  5, '28', '33', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (31,'guerreiro-atk-6',  6, '43', '28', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (32,'guerreiro-def-6',  6, '28', '43', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (33,'guerreiro-atk-7',  7, '65', '40', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (34,'guerreiro-def-7',  7, '40', '65', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (35,'guerreiro-atk-8',  8, '75', '50', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (36,'guerreiro-def-8',  8, '50', '75', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (37,'guerreiro-atk-9',  9, '90', '80', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (38,'guerreiro-def-9',  9, '80', '90', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (39,'guerreiro-atk-10', 10,'110', '100', 'Magic','0');
-INSERT INTO warriors_magic (id,warrior,lvl,atk,def,origin, total) values (40,'guerreiro-def-10', 10,'100', '110', 'Magic','0');
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (1,'Aprendizes de Eris Atk', 1,  9, 8);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (2,'Aprendizes de Eris Def', 1,  8, 10);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (3,'Aprendizes de Remus Atk', 2,  11, 4);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (4,'Aprendizes de Remus Def', 2,  9, 15);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (5,'Aprendizes de Wilfred Atk', 3,  20, 18);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (6,'Aprendizes de Wilfred Def', 3,  18, 20);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (7,'Aprendizes de Garon Atk', 4,  31, 21);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (8,'Aprendizes de Garon Def', 4,  21, 31);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (9,'Aprendizes de Jafar Atk', 5,  37, 28);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (10,'Aprendizes de Jafar Def',  5, 28, 37);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (11,'Magos de Kuron Atk',  6, 43, 28);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (12,'Magos de Kuron Def',  6, 28, 43);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (13,'Magos de Relic Atk',  7, 65, 40);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (14,'Magos de Relic Def',  7, 40, 65);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (15,'Magos de Pierce Atk',  8, 70, 50);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (16,'Magos de Pierce Def',  8, 50, 70);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (17,'Magos de Linus Atk',  9, 90, 80);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (18,'Magos de Linus Def',  9, 80, 90);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (19,'Magos Supremos Atk', 10,110, 100);
+INSERT INTO warriors_magic (id,warrior,level,atk,def) values (20,'Magos Supremos Def', 10,100, 110);
 
---------------Tabela origem Guerra
-CREATE TABLE warriors_war ( 
-    id serial NOT NULL, 
-    warrior VARCHAR(20), 
-    lvl int ,
-    atk VARCHAR(3),
-    def VARCHAR(3), 
-    origin VARCHAR(15) DEFAULT 'War', 
-    total VARCHAR(10),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS warriors_war ( 
+    id INT PRIMARY KEY, 
+    warrior VARCHAR(50), 
+    level INT NOT NULL,
+    atk INT NOT NULL,
+    def INT NOT NULL
 );
 
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (41,'guerreiro-atk-1', 1,  '15', '10', 'War', '0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (42,'guerreiro-def-1', 1,  '10', '15', 'War', '0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (43,'guerreiro-atk-2', 2,  '20', '16', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (44,'guerreiro-def-2', 2,  '16', '20', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (45,'guerreiro-atk-3', 3,  '25', '21', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (46,'guerreiro-def-3', 3,  '21', '25', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (47,'guerreiro-atk-4', 4,  '32', '30', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (48,'guerreiro-def-4', 4,  '30', '32', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (49,'guerreiro-atk-5', 5,  '40', '35', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (50,'guerreiro-def-5',  5, '35', '40', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (51,'guerreiro-atk-6',  6, '47', '42', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (52,'guerreiro-def-6',  6, '42', '47', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (53,'guerreiro-atk-7',  7, '60', '53', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (54,'guerreiro-def-7',  7, '53', '60', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (55,'guerreiro-atk-8',  8, '71', '66', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (56,'guerreiro-def-8',  8, '66', '71', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (57,'guerreiro-atk-9',  9, '85', '80', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (58,'guerreiro-def-9',  9, '80', '85', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (59,'guerreiro-atk-10', 10,'105', '98', 'War','0');
-INSERT INTO warriors_war (id,warrior,lvl,atk,def,origin, total) values (60,'guerreiro-def-10', 10,'98', '105', 'War','0');
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (1,'Guerreiros do comandante Théos de Atk', 1,  15, 10);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (2,'Guerreiros do comandante Théos de Def', 1,  10, 15);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (3,'Guerreiros do comandante Rodger de Atk', 2,  20, 16);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (4,'Guerreiros do comandante Rodger de Def', 2,  16, 20);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (5,'Guerreiros do comandante Magno de Atk', 3,  25, 21);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (6,'Guerreiros do comandante Magno de Def', 3,  21, 25);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (7,'Guerreiros do comandante Liam de Atk', 4,  32, 30);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (8,'Guerreiros do comandante Liam de Def', 4,  30, 32);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (9,'Guerreiros do comandante Apólo de Atk', 5,  40, 35);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (10,'Guerreiros do comandante Apólo de Def',  5, 35, 40);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (11,'Guerreiros do comandante Dandara de Atk',  6, 47, 42);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (12,'Guerreiros do comandante Dandara de Def',  6, 42, 47);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (13,'Guerreiros do comandante Celina de Atk',  7, 60, 53);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (14,'Guerreiros do comandante Celina de Def',  7, 53, 60);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (15,'Guerreiros do comandante Ícarus de Atk',  8, 71, 66);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (16,'Guerreiros do comandante Ícarus de Def',  8, 66, 71);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (17,'Guerreiros do comandante Orion de Atk',  9, 85, 80);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (18,'Guerreiros do comandante Orion de Def',  9, 80, 85);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (19,'Guerreiros do ELITE de Atk', 10,105, 98);
+INSERT INTO warriors_war (id,warrior,level,atk,def) values (20,'Guerreiros do ELITE de Def', 10,98, 105);
