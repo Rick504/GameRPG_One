8075292aa720f8c35e198fd903f1f960020c6c3f
Rick Wesley Rios <70915863+Rick504@users.noreply.github.com>
2022-04-05 11:12:44 -0300

    Criado uma pasta para testes em controllers

--- routes/testes.js
+++ routes/testes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const users = require("../controllers/users")
+const users = require("../controllers/testes/users")
 
 router 
     .get('/users', users.users)
