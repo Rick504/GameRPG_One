d951d075ab75c0684f767be59a6d01cb795725c8
Rick Wesley Rios <70915863+Rick504@users.noreply.github.com>
2022-04-05 17:28:01 -0300

    Iniciando testes em TS e criando packge.json para pasta ts e atualizando o mesmo packge.json no dist

--- ts/app.ts
+++ ts/app.ts
@@ -1,6 +1,8 @@
-const express = require("express")
-const session = require("express-session")
-const cors = require('cors')
+import express from "express"
+import session from "express-session"
+import { Request, Response } from 'express';
+import cors from 'cors'
+import routes from './routes';
 const app = express()
 
 
@@ -9,21 +11,20 @@ app.use(session({
     secret: "secret",
     resave: true, 
     saveUninitialized: true,
-    cookie: { 
-        key: "is8-2inD-a83-dhuw-de", // chave criada por mim
-        maxAge: 360000 // 3hrs para expirar
-    }
+    // cookie: { 
+    //     key: "is8-2inD-a83-dhuw-de", // chave criada por mim
+    //     maxAge: 360000 // 3hrs para expirar
+    // }
 }))
 
 
 // Cors - settings ---------------------------------------//
-app.use((req, res, next) => {
+app.use((req: Request, res: Response) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers": "Authorization')
     res.header('Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Content-Type": "application/json;charset=UTF-8')
     app.use(cors())
-    next()
 })
 
 
@@ -33,10 +34,7 @@ app.use(express.json())
 
 
 // Routers -------------------------------------//
-app.use('/', require('./routes/index'))
-app.use('/', require('./routes/crud'))
-app.use('/', require('./routes/authentication'))
-app.use('/', require('./routes/testes'))
+app.use(routes);
   
 
 app.listen(3333, () => {
