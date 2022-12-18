914ac7aafbd5a6bf72bc18b14b7d7bb4ec75412e
Rick504 <rick.wes1@gmail.com>
2022-12-18 16:32:52 -0300

    iniciando teste em vue.js

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="userName">
+    <input type="password" v-model="userPassword">
+    <button @click="login">Entrar</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data() {
+    return {
+      userName: null,
+      userPassword: null
+    }
+  },
+  methods: {
+    login() {
+
+    }
+  },
+}
+</script>
