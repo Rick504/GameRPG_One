454e351472a1e6cb99c11819d8fa63df0b8c4b97
Rick504 <rick.wes1@gmail.com>
2022-12-28 11:26:38 -0300

    fix: resolvendo proble de duplicate key de emails com try/catch

--- controllers/users/update_user_email.js
+++ controllers/users/update_user_email.js
@@ -8,22 +8,9 @@ const update_user_email = async (req, res) => {
         else {
 
             let userBody = req.body
-            // let isValidEmail;
-            // let email_users = await knex.select('email').from('users')
-
-            // // ARRUMAR ESSA FUNÇÃAAAAO **********************************
-            // await email_users.forEach(element => {
-            //     element.email !== userBody.email? isValidEmail = true : isValidEmail = false
-            // });
-
-            // console.log('passou o loop',isValidEmail)
 
             let email_confirm = userBody.email_confirm
             let email = userBody.email
-
-            // console.log('isValidEmail && email === email_confirm' , isValidEmail && email === email_confirm? true: false)
-            // console.log('isValidEmail' , isValidEmail? true: false)
-            // console.log('email === email_confirm' , email === email_confirm? true: false)
 
             if (email === email_confirm) {
                 if (req.session.cookie._expires !== null) {
